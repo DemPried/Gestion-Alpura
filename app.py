1b27cfe743eec989310008b6aae5154aac0edec5
DemPried <90942767+DemPried@users.noreply.github.com>
2021-10-22 17:32:48 -0500

    Add files via upload
    login funcion basica de los 3 perfiles

--- app.py
+++ app.py
@@ -1,67 +1,135 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():  # put application's code here
-    return render_template('index.html')
-"""""
-@app.route('/login')
+def index():
+    try:
+        if request.method =='POST':
+
+            username = request.form['username']
+
+            password = request.form['password']
+
+            if not username:
+                error = 'Debes ingresar un usuario'
+                flash(error)
+                return render_template('index.html')
+
+            if not password:
+                error = 'Debes ingresar una contraseña'
+                flash(error)
+                return render_template('index.html')
+
+            username == 'BACCA1' and password == '1234'
+
+            return render_template('VisualizarUsuarioFinal.html')
+        else:
+            return render_template('index.html')
+    except Exception as ex:
+        print(ex)
+        return render_template('index.html')
+
+   # return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():  # put application's code here
-    return render_template('index.html')
-"""""
+    try:
+        if request.method =='POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            if not username:
+                error = 'Debes ingresar un usuario'
+                flash(error)
+                return render_template('index.html')
+
+            if not password:
+                error = 'Debes ingresar una contraseña'
+                flash(error)
+                return render_template('index.html')
+
+            if username == 'SuperADM' and password == '1234':
+                return render_template('gestorUsuarioSuper.html')
+            if username == 'ADMIN' and password == '1234':
+                return render_template('gestorEmpleadosAdmin.html')
+            if username == 'empleado'and password == '1234':
+                return redirect(url_for('menu_empleado'))
+            else:
+                return render_template('index.html')
+        else:
+            return render_template('index.html')
+    except Exception as ex:
+        print(ex)
+        return render_template('index.html')
+
+   # return render_template('index.html')
+
 @app.route('/menu_empleado', methods=['GET', 'POST'])
 def menu_empleado():
-    return  render_template('VisualizarUsuarioFinal.html')
+    return render_template('VisualizarUsuarioFinal.html')
+
 
 @app.route('/menu_administrador', methods=['GET', 'POST'])
 def menu_administrador():
-    return  render_template('gestorEmpleadosAdmin.html')
+    return render_template('gestorEmpleadosAdmin.html')
+
 
 @app.route('/AgregarEmpleados', methods=['GET', 'POST'])
-def AgregarEmpleados():
-    return  render_template('AgregarEmpleados.html')
+def agregarEmpleados():
+    return render_template('AgregarEmpleados.html')
+
 
 @app.route('/BuscarEmpleados', methods=['GET', 'POST'])
-def BuscarEmpleados():
-    return  render_template('BuscarEmpleado.html')
+def buscarEmpleados():
+    return render_template('BuscarEmpleado.html')
+
 
 @app.route('/EditarEmpleados', methods=['GET', 'POST'])
-def EditarEmpleados():
-    return  render_template('EditarEmpleados.html')
+def editarEmpleados():
+    return render_template('EditarEmpleados.html')
+
 
 @app.route('/DesempeñoEmpleados', methods=['GET', 'POST'])
-def DesempeñoEmpleados():
-    return  render_template('DesempeñoEmpleado.html')
+def desempeñoEmpleados():
+    return render_template('DesempeñoEmpleado.html')
+
 
 @app.route('/VisualizadordesdeAdmin', methods=['GET', 'POST'])
-def VisualizadordesdeAdmin():
-    return  render_template('VisualisadordesdeAdmin.html')
+def visualizadordesdeAdmin():
+    return render_template('VisualisadordesdeAdmin.html')
+
 
 @app.route('/GestorUsuariosSuper', methods=['GET', 'POST'])
-def GestorUsuariSuper():
-    return  render_template('gestorUsuarioSuper.html')
+def gestorUsuariSuper():
+    return render_template('gestorUsuarioSuper.html')
+
 
 @app.route('/AgregarUsuarioSuper', methods=['GET', 'POST'])
-def AgregarUsuarioSuper():
-    return  render_template('AgregarUsuario.html')
+def agregarUsuarioSuper():
+    return render_template('AgregarUsuario.html')
+
 
 @app.route('/EditarUsuariosuper', methods=['GET', 'POST'])
-def EditarUsuariosuper():
-    return  render_template('EditarUsuarioSuper.html')
+def editarUsuariosuper():
+    return render_template('EditarUsuarioSuper.html')
+
 
 @app.route('/BuscaryEliminarUsuario', methods=['GET', 'POST'])
-def BuscaryEliminarUsuario():
-    return  render_template('BuscarEliminarUsuario.html')
+def buscaryEliminarUsuario():
+    return render_template('BuscarEliminarUsuario.html')
+
 
 @app.route('/DesempeñoUsuarioSuper', methods=['GET', 'POST'])
-def DesempeñoUsuarioSuper():
-    return  render_template('DesempeñodesdeSuper.html')
+def desempeñoUsuarioSuper():
+    return render_template('DesempeñodesdeSuper.html')
+
 
 @app.route('/VisualizarUsuarioSuper', methods=['GET', 'POST'])
-def VisualizarUsuarioSupe():
-    return  render_template('VisualizardesdeSuper.html')
+def visualizarUsuarioSupe():
+    return render_template('VisualizardesdeSuper.html')
+
 
 if __name__ == '__main__':
     app.run()
